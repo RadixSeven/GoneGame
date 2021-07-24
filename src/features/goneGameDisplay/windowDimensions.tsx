118813c5c9e9dbc6910769b31b032e5eb52336d9
Eric Moyer <compu.moyer@gmail.com>
2021-07-24 04:02:59 -0400

    Code for getting window dimensions

---
+++ src/features/goneGameDisplay/windowDimensions.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+/**
+ * Hook for getting window dimensions
+ *
+ * From https://stackoverflow.com/a/36862446/309334
+ * Example:
+ *
+ * const Component = () => {
+ *   const { height, width } = useWindowDimensions();
+ *   ...
+ * }
+ */
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
